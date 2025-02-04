9b2e73a847c9902fcc25a6017199aae70b6e11fe
VorsCompany <alexis.coar.cc@gmail.com>
2025-02-04 18:27:00 -0500

    add capcitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.vors.dev',
   appName: 'vorssorteos',
-  webDir: 'dist'
+  webDir: 'dist/sorteos-vors/browser'
 };
 
 export default config;
--- src/app/screen/formulario/formulario.component.ts
+++ src/app/screen/formulario/formulario.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
 import { FormsModule } from '@angular/forms';
 import { FormularioService } from '../../service/formulario.service';
-
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Platform } from '@angular/cdk/platform';
 
 @Component({
   selector: 'app-formulario',
@@ -11,10 +13,27 @@ import { FormularioService } from '../../service/formulario.service';
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
 
-  constructor(private formularioService: FormularioService) {}
-  imagenSeleccionada: File | null = null;
+export class FormularioComponent {  
+  isMobile: any;
+  constructor(private formularioService: FormularioService, public platform: Platform) {
+  }
+  
+  async solicitarPermisos() {
+    const permissions = await Camera.requestPermissions();
+    // Manejar la respuesta de permisos si es necesario
+  }
+  
+  async ngOnInit() {
+    const permissions = await Camera.requestPermissions();
+    this.isMobile = Capacitor.getPlatform();
+    console.log("1");
+    
+    console.log(this.isMobile);
+    
+  }
+  
+  imagenSeleccionada: any = null;
   imagenBase64: string | null = null;
 
   // Agrega un modelo para los campos del formulario
@@ -43,6 +62,54 @@ export class FormularioComponent {
     }
   }
 
+  async seleccionarImagen() {
+    if (Capacitor.getPlatform() === 'web') {
+      // Para la web, utiliza un input file en la plantilla HTML
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.accept = 'image/*';
+      input.onchange = (event: any) => {
+        const file = event.target.files[0];
+        if (file) {
+          this.imagenSeleccionada = file;
+
+          // Convertir la imagen a Base64 para mostrarla como vista previa
+          const reader = new FileReader();
+          reader.onload = () => {
+            this.imagenBase64 = reader.result as string;
+          };
+          reader.readAsDataURL(this.imagenSeleccionada);
+        }
+      };
+      input.click();
+    } else {
+      //pedir permisosen los celulares para acceder asus alamacenamiento
+    const permissions = await Camera.requestPermissions();
+      // Para plataformas nativas, utiliza el plugin de la cámara
+      try {
+        const image = await Camera.getPhoto({
+          quality: 90,
+          allowEditing: false,
+          resultType: CameraResultType.Uri,
+          source: CameraSource.Photos // Esto abre la galería de fotos
+        });
+
+        if (image.webPath) {
+          // Mostrar la imagen seleccionada en la interfaz
+          this.imagenBase64 = image.webPath;
+
+          // Convertir la imagen a un archivo para enviarla en el formulario
+          const response = await fetch(image.webPath);
+          const blob = await response.blob();
+          this.imagenSeleccionada = new File([blob], 'imagen.jpg', { type: blob.type });
+        }
+      } catch (error) {
+        console.error('Error al seleccionar imagen:', error);
+      }
+    }
+  }
+
+    
   eliminarImagen(): void {
     this.imagenSeleccionada = null;
     this.imagenBase64 = null;
@@ -56,10 +123,13 @@ export class FormularioComponent {
     formData.append('telefono', this.formularioData.telefono);
     formData.append('direccion', this.formularioData.direccion);
     formData.append('numeroConfirmacion', this.formularioData.numeroConfirmacion);
-
+  
     if (this.imagenSeleccionada) {
       formData.append('imagen', this.imagenSeleccionada);
-      this.formularioService.cargarFomulario(formData)  
     }
-  }  
+  
+    this.formularioService.cargarFomulario(formData);
+  }
+  
+  
 }
